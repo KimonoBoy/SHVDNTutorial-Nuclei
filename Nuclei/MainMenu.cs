06d01c6bd3686f90d5691f4d610637c1af7a9d21
KimonoBoy <simo17k5@easv365.dk>
2023-03-06 00:03:05 +0100

    Added Fix Player Item

--- Nuclei/MainMenu.cs
+++ Nuclei/MainMenu.cs
@@ -1,8 +1,15 @@
-﻿namespace Nuclei;
+﻿using GTA;
+
+namespace Nuclei;
 
 public class MainMenu : MenuBase
 {
     public MainMenu(string subtitle, string description) : base(subtitle, description)
     {
+        AddItem("Fix Player", "Restores Player's Health and Armor back to full.", () =>
+        {
+            Game.Player.Character.Health = Game.Player.Character.MaxHealth;
+            Game.Player.Character.Armor = Game.Player.MaxArmor;
+        });
     }
 }
