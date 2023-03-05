15542c3a05f31867c1900b73e45763105e4086ce
KimonoBoy <simo17k5@easv365.dk>
2023-03-05 15:43:39 +0100

    Fix Player Item

--- Nuclei/Main.cs
+++ Nuclei/Main.cs
@@ -8,14 +8,24 @@ namespace Nuclei;
 
 public class Main : Script
 {
+    private readonly NativeItem _itemFixPlayer = new("Fix Player", "Restores Player's Health and Armor.");
     private readonly NativeMenu _menu = new("Nuclei", "Main Menu");
     private readonly ObjectPool _pool = new();
 
     public Main()
     {
         _pool.Add(_menu);
+
+        _itemFixPlayer.Activated += OnFixPlayerActivated;
+        _menu.Add(_itemFixPlayer);
         KeyDown += OnKeyDown;
         Tick += OnTick;
+    }
+
+    private void OnFixPlayerActivated(object sender, EventArgs e)
+    {
+        Game.Player.Character.Health = Game.Player.Character.MaxHealth;
+        Game.Player.Character.Armor = Game.Player.MaxArmor;
     }
 
     private void OnTick(object sender, EventArgs e)
