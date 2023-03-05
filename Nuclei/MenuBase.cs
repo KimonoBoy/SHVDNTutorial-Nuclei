da62f774e43215edc70f6d56008839a628fdfc28
KimonoBoy <simo17k5@easv365.dk>
2023-03-05 22:47:07 +0100

    MenuBase Updated with AddItem

--- Nuclei/MenuBase.cs
+++ Nuclei/MenuBase.cs
@@ -1,10 +1,34 @@
-﻿using LemonUI.Menus;
+﻿using System;
+using LemonUI;
+using LemonUI.Menus;
 
 namespace Nuclei;
 
 public abstract class MenuBase : NativeMenu
 {
+    public static ObjectPool MenuPool = new();
+
     protected MenuBase(string subtitle, string description) : base("Nuclei", subtitle, description)
     {
+        MenuPool.Add(this);
+    }
+
+    /// <summary>
+    ///     Adds a new item to the menu.
+    /// </summary>
+    /// <param name="text">The 'title' of the item.</param>
+    /// <param name="description">The description when the item is selected.</param>
+    /// <param name="action">The action to perform when activated.</param>
+    /// <returns>The item.</returns>
+    protected NativeItem AddItem(string text, string description = "", Action action = null)
+    {
+        var item = new NativeItem(text, description);
+        item.Activated += (sender, args) =>
+        {
+            if (action != null)
+                action();
+        };
+        Add(item);
+        return item;
     }
 }
