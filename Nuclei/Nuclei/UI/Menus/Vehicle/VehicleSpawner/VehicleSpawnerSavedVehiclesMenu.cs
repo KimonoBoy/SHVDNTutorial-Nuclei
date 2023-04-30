4ec7bbf7b2c4049e8ef8d0c4f4d72b44060ddea5
KimonoBoy <simo17k5@easv365.dk>
2023-04-30 03:00:00 +0200

    Updated Save Vehicle to save the vehicle without auto-save or hitting CTRL + S

--- Nuclei/Nuclei/UI/Menus/Vehicle/VehicleSpawner/VehicleSpawnerSavedVehiclesMenu.cs
+++ Nuclei/Nuclei/UI/Menus/Vehicle/VehicleSpawner/VehicleSpawnerSavedVehiclesMenu.cs
@@ -8,7 +8,6 @@ using GTA.UI;
 using LemonUI.Scaleform;
 using Nuclei.Enums.UI;
 using Nuclei.Helpers.ExtensionMethods;
-using Nuclei.Services.Exception;
 using Nuclei.Services.Vehicle.VehicleMods;
 using Nuclei.Services.Vehicle.VehicleSpawner;
 
@@ -22,47 +21,59 @@ public class VehicleSpawnerSavedVehiclesMenu : VehicleSpawnerMenuBase
 
     private string GetModdedVehicleDescription(CustomVehicleDto vehicleDto)
     {
+        if (vehicleDto == null) throw new ArgumentNullException(nameof(vehicleDto));
+
         var spawnTitle = $"Spawn: {vehicleDto.VehicleHash.GetLocalizedDisplayNameFromHash()}";
         return $"{spawnTitle}";
     }
 
     protected override void UpdateMenuItems<T>(IEnumerable<T> newItems)
     {
+        if (newItems == null) throw new ArgumentNullException(nameof(newItems));
+
         Clear();
         SaveCurrentVehicle();
-        foreach (var customVehicle in (ObservableCollection<CustomVehicleDto>)newItems)
-        {
-            var itemSpawnCustomVehicle =
-                AddItem(customVehicle.Title,
-                    $"{GetModdedVehicleDescription(customVehicle)}",
-                    () => { Service.SpawnVehicle(customVehicle); });
-        }
+
+        if (newItems is ObservableCollection<CustomVehicleDto> customVehicles)
+            foreach (var customVehicle in customVehicles)
+                AddCustomVehicleItem(customVehicle);
+        else
+            throw new ArgumentException("Invalid collection type.", nameof(newItems));
     }
 
     protected override void OnVehicleCollectionChanged<T>(object sender, NotifyCollectionChangedEventArgs e)
     {
+        if (e == null) throw new ArgumentNullException(nameof(e));
+
         switch (e.Action)
         {
             case NotifyCollectionChangedAction.Add when e.NewItems != null:
-                e.NewItems.Cast<CustomVehicleDto>().ToList().ForEach(customVehicle =>
-                {
-                    var itemVehicle = AddItem(customVehicle.Title,
-                        $"{GetModdedVehicleDescription(customVehicle)}",
-                        () => { Service.SpawnVehicle(customVehicle); });
-                });
+                e.NewItems.Cast<CustomVehicleDto>().ToList().ForEach(AddCustomVehicleItem);
                 break;
             case NotifyCollectionChangedAction.Remove when e.OldItems != null:
-                e.OldItems.Cast<CustomVehicleDto>().ToList().ForEach(customVehicle =>
-                {
-                    var item = Items.FirstOrDefault(i => i.Title == customVehicle.Title);
-                    if (item != null)
-                    {
-                        var itemIndex = Items.IndexOf(item);
-                        Remove(item);
-                        if (Items.Count > 0) SelectedIndex = Math.Max(0, Math.Min(itemIndex, Items.Count - 1));
-                    }
-                });
+                e.OldItems.Cast<CustomVehicleDto>().ToList().ForEach(RemoveCustomVehicleItem);
                 break;
+        }
+    }
+
+    private void AddCustomVehicleItem(CustomVehicleDto customVehicle)
+    {
+        if (customVehicle == null) throw new ArgumentNullException(nameof(customVehicle));
+
+        AddItem(customVehicle.Title, $"{GetModdedVehicleDescription(customVehicle)}",
+            () => Service.SpawnVehicle(customVehicle));
+    }
+
+    private void RemoveCustomVehicleItem(CustomVehicleDto customVehicle)
+    {
+        if (customVehicle == null) throw new ArgumentNullException(nameof(customVehicle));
+
+        var item = Items.FirstOrDefault(i => i.Title == customVehicle.Title);
+        if (item != null)
+        {
+            var itemIndex = Items.IndexOf(item);
+            Remove(item);
+            if (Items.Count > 0) SelectedIndex = Math.Max(0, Math.Min(itemIndex, Items.Count - 1));
         }
     }
 
@@ -74,84 +85,75 @@ public class VehicleSpawnerSavedVehiclesMenu : VehicleSpawnerMenuBase
 
     protected override void UpdateSelectedItem(string title)
     {
+        if (string.IsNullOrEmpty(title)) throw new ArgumentNullException(nameof(title));
+
         var vehicleHash = Service.CustomVehicles.FirstOrDefault(v => v.Title == title)?.VehicleHash;
 
-        if (vehicleHash != null)
-            Service.CurrentVehicleHash = (VehicleHash)vehicleHash;
+        if (vehicleHash != null) Service.CurrentVehicleHash = (VehicleHash)vehicleHash;
     }
 
     private void SaveCurrentVehicle()
     {
-        var itemSaveCurrentVehicle = AddItem(VehicleSpawnerItemTitle.SaveCurrentVehicle,
-            () =>
+        AddItem(VehicleSpawnerItemTitle.SaveCurrentVehicle, () =>
+        {
+            if (Service.CurrentVehicle == null)
             {
-                try
-                {
-                    /*
-                 * Due for later. Lets finish the Modicfications Menu first before updating this.
-                 */
-                    if (Service.CurrentVehicle == null)
-                    {
-                        Notification.Show("You must enter a vehicle first.");
-                        return;
-                    }
+                Notification.Show("You must enter a vehicle first.");
+                return;
+            }
 
-                    var userInput = Game.GetUserInput(WindowTitle.EnterMessage60, "", 60);
+            var userInput = Game.GetUserInput(WindowTitle.EnterMessage60, "", 60);
 
-                    if (Service.CustomVehicles.Any(v =>
-                            v.Title == userInput))
-                    {
-                        Notification.Show("Vehicle with that title already exists. Please enter a unique title.");
-                        return;
-                    }
+            if (Service.CustomVehicles.Any(v => v.Title == userInput))
+            {
+                Notification.Show("Vehicle with that title already exists. Please enter a unique title.");
+                return;
+            }
 
-                    if (string.IsNullOrEmpty(userInput))
-                    {
-                        Notification.Show("Please enter a title to save the vehicle.");
-                        return;
-                    }
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Notification.Show("Please enter a title to save the vehicle.");
+                return;
+            }
 
-                    var customVehicle = new CustomVehicleDto
-                    {
-                        Title = userInput,
-                        VehicleHash = (VehicleHash)Service.CurrentVehicle.Model.Hash,
+            var customVehicle = new CustomVehicleDto
+            {
+                Title = userInput,
+                VehicleHash = (VehicleHash)Service.CurrentVehicle.Model.Hash,
 
-                        LicensePlate = VehicleModsService.Instance.LicensePlate,
-                        LicensePlateStyle = VehicleModsService.Instance.LicensePlateStyle,
+                LicensePlate = VehicleModsService.Instance.LicensePlate,
+                LicensePlateStyle = VehicleModsService.Instance.LicensePlateStyle,
 
-                        WheelType = VehicleModsService.Instance.WheelType,
-                        CustomTires = VehicleModsService.Instance.CustomTires,
-                        RimColor = VehicleModsService.Instance.RimColor,
+                WheelType = VehicleModsService.Instance.WheelType,
+                CustomTires = VehicleModsService.Instance.CustomTires,
+                RimColor = VehicleModsService.Instance.RimColor,
 
-                        PrimaryColor = VehicleModsService.Instance.PrimaryColor,
-                        SecondaryColor = VehicleModsService.Instance.SecondaryColor,
-                        PearlescentColor = VehicleModsService.Instance.PearlescentColor,
+                PrimaryColor = VehicleModsService.Instance.PrimaryColor,
+                SecondaryColor = VehicleModsService.Instance.SecondaryColor,
+                PearlescentColor = VehicleModsService.Instance.PearlescentColor,
 
-                        NeonLightsLayout = VehicleModsService.Instance.NeonLightsLayout,
-                        NeonLightsColor = VehicleModsService.Instance.NeonLightsColor,
+                NeonLightsLayout = VehicleModsService.Instance.NeonLightsLayout,
+                NeonLightsColor = VehicleModsService.Instance.NeonLightsColor,
 
-                        WindowTint = VehicleModsService.Instance.WindowTint,
+                WindowTint = VehicleModsService.Instance.WindowTint,
 
-                        XenonHeadLights = VehicleModsService.Instance.XenonHeadLights,
-                        Turbo = VehicleModsService.Instance.Turbo,
+                XenonHeadLights = VehicleModsService.Instance.XenonHeadLights,
+                Turbo = VehicleModsService.Instance.Turbo,
 
-                        TireSmokeColor = VehicleModsService.Instance.TireSmokeColor,
-                        RainbowMode = VehicleModsService.Instance.RainbowMode
-                    };
+                TireSmokeColor = VehicleModsService.Instance.TireSmokeColor,
+                RainbowMode = VehicleModsService.Instance.RainbowMode
+            };
 
-                    foreach (var vehicleMod in Service.CurrentVehicle.Mods.ToArray())
-                    {
-                        var customVehicleMod = new CustomVehicleModDto(vehicleMod.Type, vehicleMod.Index);
-                        customVehicle.VehicleMods.Add(customVehicleMod);
-                    }
+            foreach (var vehicleMod in Service.CurrentVehicle.Mods.ToArray())
+            {
+                var customVehicleMod = new CustomVehicleModDto(vehicleMod.Type, vehicleMod.Index);
+                customVehicle.VehicleMods.Add(customVehicleMod);
+            }
 
-                    Service.CustomVehicles.Add(customVehicle);
-                }
-                catch (Exception e)
-                {
-                    ExceptionService.Instance.RaiseError(e);
-                }
-            });
+            Service.CustomVehicles.Add(customVehicle);
+            Service.GetStateService().SetState(Service);
+            Service.GetStateService().SaveState();
+        });
     }
 
     protected override void AddButtons()
